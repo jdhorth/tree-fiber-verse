e9a35f5d5dac8145cf36d9a8ab2a43ff3ca2990e
jdhorth <85119999+jdhorth@users.noreply.github.com>
2022-10-02 03:36:03 -0400

    Update Lights.tsx

--- components/Lights.tsx
+++ components/Lights.tsx
@@ -3,7 +3,7 @@ import { useRef } from "react"
 import { DirectionalLightHelper } from "three"
 
 const Lights: React.FC = () => {
-    const lightRef = useRef<THREE.DirectionalLight>(null);
+    const lightRef = useRef<THREE.DirectionalLight>(undefined);
 
     useHelper(lightRef, DirectionalLightHelper, 5, "red")
 
